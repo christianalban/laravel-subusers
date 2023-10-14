91e740dbc0b5681e38bab6a5ba51dc6a675631fa
Christian Albán <chris_alban@live.com>
2023-10-14 16:51:43 -0500

    include migrations on boot

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -8,12 +8,14 @@ class ServiceProvider extends Support\ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../config/owners.php' => config_path('owners.php'),
+            __DIR__.'/config/owners.php' => config_path('owners.php'),
         ], 'config');
+
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
     }
 
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/owners.php', 'owners');
+        $this->mergeConfigFrom(__DIR__.'/config/owners.php', 'owners');
     }
 }
